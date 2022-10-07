afc0b605f0683561ec243bb30545de11ce2d74af
KevinEscobarDeveloper <89361885+KevinEscobarDeveloper@users.noreply.github.com>
2022-10-07 12:31:34 -0500

    modifications

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+var cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
@@ -21,6 +22,7 @@ moongose.connect(process.env.DatabaseConnection)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
